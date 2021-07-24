37795dc8c07698d55d47df7e265d2a5493fac7f2
LoganYeu <73202655+LoganYeu@users.noreply.github.com>
2021-07-24 18:05:54 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name;
+    private int year;
+    private int ID;
+
+
+    public Student(String name, int year, int ID) {
+        this.name = name;
+        this.year = year;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+
+}
